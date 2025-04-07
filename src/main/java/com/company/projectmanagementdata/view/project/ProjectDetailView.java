2589936c57c243466dbcd61de02922c3b1948743
vashenkoda <d.vaschenko@haulmont.net>
2025-04-07 17:59:28 +0300

    Initial commit

---
+++ src/main/java/com/company/projectmanagementdata/view/project/ProjectDetailView.java
@@ -0,0 +1,16 @@
+package com.company.projectmanagementdata.view.project;
+
+import com.company.projectmanagementdata.entity.Project;
+import com.company.projectmanagementdata.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.EditedEntityContainer;
+import io.jmix.flowui.view.StandardDetailView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@Route(value = "projects/:id", layout = MainView.class)
+@ViewController(id = "Project.detail")
+@ViewDescriptor(path = "project-detail-view.xml")
+@EditedEntityContainer("projectDc")
+public class ProjectDetailView extends StandardDetailView<Project> {
+}
---
+++ src/main/java/com/company/projectmanagementdata/view/project/ProjectListView.java
@@ -0,0 +1,15 @@
+package com.company.projectmanagementdata.view.project;
+
+import com.company.projectmanagementdata.entity.Project;
+import com.company.projectmanagementdata.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+
+@Route(value = "projects", layout = MainView.class)
+@ViewController(id = "Project.list")
+@ViewDescriptor(path = "project-list-view.xml")
+@LookupComponent("projectsDataGrid")
+@DialogMode(width = "64em")
+public class ProjectListView extends StandardListView<Project> {
+}
